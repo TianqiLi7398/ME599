2bfd89f875986b1adef4fe8df6fed626f2bf6612
Tony <tq@D-108-179-151-17.dhcp4.washington.edu>
2018-03-07 16:53:02 -0800

    divide

--- scratch.py
+++ scratch.py
@@ -7,144 +7,41 @@ import numpy as np
 from skimage import measure
 import plotUtils
 
-TPBX = 8
-TPBY = 8
-TPBZ = 8
-@cuda.jit(device=True)
-def f_heart(x,y,z):
-    return 320 * ((-x**2 * z**3 -9*y**2 * z**3/80) + \
-                  (x**2 + 9*y**2/4 + z**2-1)**3)
-
-@cuda.jit
-def f_heart_kernel(d_f, d_x, d_y, d_z):
-    i, j, k = cuda.grid(3)
-    nx, ny, nz = d_f.shape
-    if i < nx and j < ny and k < nz:
-        d_f[i,j,k] = f_heart(d_x[i,j,k], d_y[i,j,k], d_z[i,j,k])
-
-def f_heart3D(X, Y, Z):
-    nx, ny, nz = X.shape
-
-    d_x = cuda.to_device(X)
-    d_y = cuda.to_device(Y)
-    d_z = cuda.to_device(Z)
-    d_f = cuda.device_array((nx,ny,nz), dtype=np.float32)
-
-    gridDims = ((nx + TPBX - 1) // TPBX,
-                (ny + TPBY - 1) // TPBY,
-                (nz + TPBZ - 1) // TPBZ
-                )
-    blockDims = (TPBX, TPBY, TPBZ)
-    f_heart_kernel[gridDims, blockDims](d_f, d_x, d_y, d_z)
-    return d_f.copy_to_host()
+import util as *
 
 
-@cuda.jit(device=True)
-def f_sphere(x,y,z,R):
-    return 1 * (x**2 + y**2 + z**2 - R**2)
-
-@cuda.jit
-def f_sphere_kernel(d_f, d_x, d_y, d_z, R):
-    i, j, k = cuda.grid(3)
-    nx, ny, nz = d_f.shape
-    if i < nx and j < ny and k < nz:
-        d_f[i,j,k] = f_sphere(d_x[i,j,k], d_y[i,j,k], d_z[i,j,k], R)
-
-def f_sphere3D(X, Y, Z, R):
-    nx, ny, nz = X.shape
-
-    d_x = cuda.to_device(X)
-    d_y = cuda.to_device(Y)
-    d_z = cuda.to_device(Z)
-    d_f = cuda.device_array((nx,ny,nz), dtype=np.float32)
-
-    gridDims = ((nx + TPBX - 1) // TPBX,
-                (ny + TPBY - 1) // TPBY,
-                (nz + TPBZ - 1) // TPBZ
-                )
-    blockDims = (TPBX, TPBY, TPBZ)
-    f_sphere_kernel[gridDims, blockDims](d_f, d_x, d_y, d_z, R)
-    return d_f.copy_to_host()
-
-def morph(shape_start, shape_end, t):
-    n = len(shape_start)
-    rst = np.zeros((n,n,n), dtype=np.float32)
-    for ix in range(n):
-        for iy in range(n):
-            for iz in range(n):
-                rst[ix,iy,iz] = shape_start[ix,iy,iz] * (1-t) + t * shape_end[ix,iy,iz]
-    return rst
-
-@cuda.jit
-def morph_kernel(d_f, d_x, d_y, t):
-    i, j, k = cuda.grid(3)
-    nx, ny, nz = d_f.shape
-    if i < nx and j < ny and k < nz:
-        d_f[i,j,k] = d_x[i,j,k] * (1-t) + t * d_y[i,j,k]
-
-def morph3D(shape_start, shape_end, t):
-    nx, ny, nz = shape_start.shape
-
-    d_x = cuda.to_device(shape_start)
-    d_y = cuda.to_device(shape_end)
-    d_f = cuda.device_array((nx,ny,nz), dtype=np.float32)
-
-    gridDims = ((nx + TPBX - 1) // TPBX,
-                (ny + TPBY - 1) // TPBY,
-                (nz + TPBZ - 1) // TPBZ
-                )
-    blockDims = (TPBX, TPBY, TPBZ)
-    morph_kernel[gridDims, blockDims](d_f, d_x, d_y, t)
-    return d_f.copy_to_host()
-
-
-
-def exportPLY(filename, verts2, faces):
-    plyf = open(filename +'.ply', 'w')
-    plyf.write( "ply\n")
-    plyf.write( "format ascii 1.0\n")
-    plyf.write( "comment ism.py generated\n")
-    plyf.write( "element vertex " + str(verts2.size//3)+'\n')
-    plyf.write( "property float x\n")
-    plyf.write( "property float y\n")
-    plyf.write( "property float z\n")
-    plyf.write( "element face " + str(faces.size//3)+'\n')
-    plyf.write( "property list uchar int vertex_indices\n")
-    plyf.write( "end_header\n")
-    for i in range(0,verts2.size//3):
-        plyf.write(str(verts2[i][0])+' '+str(verts2[i][1])+' '+str(verts2[i][2])+'\n')
-
-    for i in range(0,faces.size//3):
-        plyf.write('3 '+str(faces[i][0])+' '+str(faces[i][1])+' '+str(faces[i][2])+'\n') 
-    plyf.close()
-    # end of PLY file write
-    
 def main():
     # Set up mesh
-    n = 100 
+    n = 100
     R = 3.0
     m = 3.0
     dx = 2 * m / (n - 1)
     dy = 2 * m / (n - 1)
     dz = 2 * m / (n - 1)
-    x = np.linspace(-m,m,n)
-    y = np.linspace(-m,m,n)
-    z = np.linspace(-m,m,n)
-    X, Y, Z =  np.meshgrid(x, y, z)
-    heart = f_heart3D(X,Y,Z)
-    sphere = f_sphere3D(X,Y,Z,R)
+    x = np.linspace(-m, m, n)
+    y = np.linspace(-m, m, n)
+    z = np.linspace(-m, m, n)
+    X, Y, Z = np.meshgrid(x, y, z)
+
+    # CUDA
+    # heart = f_heart3D(X, Y, Z)
+    # sphere = f_sphere3D(X, Y, Z, R)
+
+    # without cuda
+    heart = heart(X, Y, Z)
+    sphere = sphere(X, Y, Z, R)
 
     surf = morph3D(sphere, heart, 0.90)
-    
+
     # Extract a 2D surface mesh from a 3D volume (F=0)
     verts, faces = measure.marching_cubes_classic(surf, 0.0, spacing=(0.1, 0.1, 0.1))
 
     # Create a 3D figure
-    fig = plt.figure(figsize=(12,8))
+    fig = plt.figure(figsize=(12, 8))
     ax = fig.add_subplot(111, projection='3d')
 
     # Plot the surface
-    ax.plot_trisurf(verts[:, 0], verts[:,1], faces, verts[:, 2],
+    ax.plot_trisurf(verts[:, 0], verts[:, 1], faces, verts[:, 2],
                     cmap='Spectral', lw=1)
 
     # Change the angle of view and title
@@ -159,9 +56,10 @@ def main():
     plotUtils.arraycontourplot3d(surf, x, y, z, dx, dy, dz, vars=['x', 'y', 'z'], titlestring="morph3D", filename="morph3D")
     '''
     filename = 'shit'
-    
-    print('Object exported to '+filename+'.ply')
+
+    print('Object exported to ' + filename + '.ply')
     exportPLY(filename, verts, faces)
+
 
 if __name__ == '__main__':
     main()
--- testPlotUtils.py
+++ testPlotUtils.py
@@ -7,40 +7,82 @@ Execution produces (in order):
 3) 3d contour plot (and surface tesselation)
 '''
 import numpy as np
-import seaborn; seaborn.set()
+import seaborn
+seaborn.set()
 from plotUtils import *
 
 # create test functions for superellipse and superellipsoid
-def superellipse(x,y,n):
-    return x**n+y**n-1
 
-def superellipsoid(x,y,z,n):
-    return x**n+y**n+z**n-1
 
-def first(x,y,z):
-    return x**3 + y**2 -z**2
+def superellipse(x, y, n):
+    return x**n + y**n - 1
 
-def gravity(x,y,z):
+
+def superellipsoid(x, y, z, n):
+    return x**n + y**n + z**n - 3
+
+
+def heart(x, y, z):
+    return 320 * ((-x**2 * z**3 - 9 * y**2 * z**3 / 80) +
+                  (x**2 + 9 * y**2 / 4 + z**2 - 1)**3)
+
+
+def paraboloid_elip(x, y, z):
+    return z - x**2 / 1 - y**2 / 2
+
+
+def Hyperboloid(x, y, z):
+    return z - x**2 / 1 + y ** 2 / 2
+
+
+def box(x, y, z):
+    return x**10 + y ** 10 + z**10 - 1**10
+
+
+def first(x, y, z):
+    return x**3 + y**2 - z**2
+
+
+def gravity(x, y, z):
     G = 6.674e-11
     M = 1989100e24
     m = 5.9736e24
-    Ear = 92.96 # unit million miles
-    sun = -G*M*m/np.sqrt(x**2 + y**2 + z**2)
-    earth = - G*M*m/np.sqrt((x-92.96)**2 + y**2 + z**2)
-    
-    c = -G*M*m/np.sqrt(50**2 + 0**2 + 0**2)  - G*M*m/np.sqrt((50-92.96)**2 + 0**2 + 0**2)
+    Ear = 92.96  # unit million miles
+    sun = -G * M * m / np.sqrt(x**2 + y**2 + z**2)
+    earth = - G * M * m / np.sqrt((x - 92.96)**2 + y**2 + z**2)
+
+    c = -G * M * m / np.sqrt(50**2 + 0**2 + 0**2) - G * M * m / \
+        np.sqrt((50 - 92.96)**2 + 0**2 + 0**2)
     return sun + earth - c
 
-def compose(x,y,z, t):
-    return t*first(x,y,z) + (1-t)*superellipsoid(x,y,z,2)
+
+def compose(x, y, z, t):
+    return t * box(x, y, z) + (1 - t) * paraboloid_elip(x, y, z)
+
 
 def main():
-    m = 100 # min and max coordinate values
-    num = 110 # number of points along each axis
+    m = 3  # min and max coordinate values
+    num = 110  # number of points along each axis
     # Map onto a 2D grid
-    xvals = np.linspace(-m,m,num)
-    yvals = np.linspace(-m,m,num)
-    
+    xvals = np.linspace(-m, m, num)
+    yvals = np.linspace(-m, m, num)
+    zvals = np.linspace(-m, m, num)
+
+    f4 = np.zeros([num, num, num])
+
+    for t in range(10):
+        for k in range(num):
+            for j in range(num):
+                for i in range(num):
+                    f4[k, j, i] = compose(xvals[i], yvals[j], zvals[k], t * 0.1)
+        # create contourplot based on 3d array
+        # include option arg filename='fname' to export in PLY format
+        dx = 2 * m / (num - 1)
+        dy = 2 * m / (num - 1)
+        dz = 2 * m / (num - 1)
+        arraycontourplot3d(f4, xvals, yvals, zvals, dx, dy, dz, levels=[-1000, 0],
+                           titlestring='Testing arraycontourplot3d', filename='box_elp%d' % t)
+
     '''
 
     # create 2d array and fill with function evaluated on 2d grid
@@ -53,7 +95,7 @@ def main():
     # plot3d(f2,xvals, yvals,titlestring='Testing plot3d')
 
     # create contourplot based on 2d array
-    # arraycontourplot(f2,xvals, yvals, levels=[-1000,0], 
+    # arraycontourplot(f2,xvals, yvals, levels=[-1000,0],
         # titlestring='Testing arraycontourplot', filled=True)
 
     # map onto 3d grid
@@ -69,7 +111,7 @@ def main():
     dx = 2*m/(num-1)
     dy = 2*m/(num-1)
     dz = 2*m/(num-1)
-    arraycontourplot3d(f1,xvals, yvals, zvals,dx,dy,dz, levels=[-1000,0], 
+    arraycontourplot3d(f1,xvals, yvals, zvals,dx,dy,dz, levels=[-1000,0],
         titlestring='Testing arraycontourplot3d')
 
 
@@ -84,7 +126,7 @@ def main():
     dx = 2*m/(num-1)
     dy = 2*m/(num-1)
     dz = 2*m/(num-1)
-    arraycontourplot3d(f2,xvals, yvals, zvals,dx,dy,dz, levels=[-1000,0], 
+    arraycontourplot3d(f2,xvals, yvals, zvals,dx,dy,dz, levels=[-1000,0],
         titlestring='Testing arraycontourplot3d')
 
 
@@ -99,32 +141,19 @@ def main():
     dx = 2*m/(num-1)
     dy = 2*m/(num-1)
     dz = 2*m/(num-1)
-    arraycontourplot3d(f3,xvals, yvals, zvals,dx,dy,dz, levels=[-1000,0], 
+    arraycontourplot3d(f3,xvals, yvals, zvals,dx,dy,dz, levels=[-1000,0],
         titlestring='Testing arraycontourplot3d')
-    
-    
+
+
     # create contourplot based on 3d array
     # include option arg filename='fname' to export in PLY format
     dx = 2*m/(num-1)
     dy = 2*m/(num-1)
     dz = 2*m/(num-1)
-    arraycontourplot3d(f2,xvals, yvals, zvals,dx,dy,dz, levels=[-1000,0], 
+    arraycontourplot3d(f2,xvals, yvals, zvals,dx,dy,dz, levels=[-1000,0],
         titlestring='Testing arraycontourplot3d')
 '''
 
-    zvals = np.linspace(-m,m,num)
-    f4 = np.zeros([num,num,num])
-    for k in range(num):
-        for j in range(num):
-            for i in range(num):
-                f4[k,j,i] = gravity(xvals[i],yvals[j],zvals[k])
-    # create contourplot based on 3d array
-    # include option arg filename='fname' to export in PLY format
-    dx = 2*m/(num-1)
-    dy = 2*m/(num-1)
-    dz = 2*m/(num-1)
-    arraycontourplot3d(f4,xvals, yvals, zvals,dx,dy,dz, levels=[-1000,0], 
-        titlestring='Testing arraycontourplot3d')
 
 if __name__ == '__main__':
     main()
