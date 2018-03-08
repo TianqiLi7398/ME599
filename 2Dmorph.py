566f55bf840d80fade6c7a45c14b4c298e941515
Junlin Li <jlli0410@uw.edu>
2018-03-08 11:26:15 -0800

    add 2D shape morph

---
+++ 2Dmorph.py
@@ -0,0 +1,133 @@
+import math
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import pyplot as plt
+import numpy as np
+from skimage import measure
+
+# Create cardioid function 
+def f_heart(x,y,z):
+    F = 320 * ((-x**2 * z**3 -9*y**2 * z**3/80) +
+               (x**2 + 9*y**2/4 + z**2-1)**3)
+    return F
+
+# Obtain value to at every point in mesh
+# vol = f_heart(X,Y,Z)
+# a cube
+def f_cross(x, y):
+    n = len(x)
+    F = np.zeros((n,n), dtype=np.float32)
+    for ix in range(n):
+        for iy in range(n):
+            if (abs(abs(x[ix,iy]) - 40.0) < 1e-16):
+                if (abs(y[ix,iy]) <= 9.0):
+                    F[ix, iy] = 0.0
+                else:
+                    F[ix, iy] = 40.0
+            
+            elif (9.0 < abs(x[ix,iy]) < 40.0):
+                if (abs(9.0 - abs(y[ix, iy])) < 1e-16):
+                    F[ix, iy] = 0.0
+                else:
+                    F[ix, iy] = 40.0
+
+            elif (abs(abs(x[ix,iy]) - 9.0) < 1e-16):
+                if (9.0 <= abs(y[ix,iy]) <= 40.0):
+                    F[ix, iy] = 0.0
+                else:
+                    F[ix, iy] = 40.0
+            
+            elif (abs(x[ix,iy]) < 9.0):
+                if (abs(abs(y[ix, iy]) - 40.0) < 1e-16):
+                    F[ix, iy] = 0.0
+                else:
+                    F[ix, iy] = 40.0
+    return F
+
+def arraycontourplot(fvals, xvals, yvals, levels=[-1000,0], vars=['x','y'], 
+    titlestring='', filled=False):
+    fig = plt.figure()
+    X,Y = np.meshgrid(xvals,yvals)
+    if filled==True:
+        cp = plt.contourf(X, Y, fvals, levels) #, linestyles='dashed')
+    else:
+        cp = plt.contour(X, Y, fvals, levels) #, linestyles='dashed')
+    # plt.clabel(cp, inline=True, fontsize=10)
+    plt.title(titlestring)
+    plt.xlabel(vars[0])
+    plt.ylabel(vars[1])
+    plt.axis('square')
+    plt.show()
+    return cp
+
+def f_cycle(x,y,R):
+    n = len(x)
+    F = np.zeros((n,n), dtype=np.float32)
+    for ix in range(n):
+        for iy in range(n):
+            F[ix,iy] = x[ix,iy]**2 + y[ix,iy]**2 - R**2
+    return F
+
+def f_square(x,y,R):
+    n = len(x)
+    F = np.zeros((n,n), dtype=np.float32)
+    for ix in range(n):
+        for iy in range(n):
+            F[ix,iy] = x[ix,iy]**12 + y[ix,iy]**12 - R**2
+    return F
+# surf = vol
+
+def morph(shape_start, shape_end, t, final_shape):
+    n = len(shape_start)
+    for iz in range(n):
+        it = t[iz]
+        for ix in range(n):
+            for iy in range(n):
+                    final_shape[ix,iy,iz] = shape_start[ix,iy] * (1-it) + it * shape_end[ix,iy]
+    return final_shape
+
+def main():
+# Set up mesh
+    n = 801
+    m = 40
+
+    x = np.linspace(-m,m,n)
+    y = np.linspace(m,-m,n)
+    X, Y=  np.meshgrid(x, y)
+    cross = f_cross(X, Y)
+    # arraycontourplot(cross, x, y, levels=[-1,1])
+
+    R = 40.0
+    x_cycle = np.linspace(-m,m,n)
+    y_cycle = np.linspace(m,-m,n)
+    X_cycle, Y_cycle = np.meshgrid(x_cycle, y_cycle)
+    cycle = f_cycle(X_cycle, Y_cycle, R)
+    # square = f_square(X, Y, R)
+    # arraycontourplot(square, x, y, levels=[0])
+    t = np.linspace(0,10,100)
+    morph_shape = np.zeros((n,n,n), dtype=np.float32)
+    morph_shape = morph(cross, cycle, t, morph_shape)
+    surf = morph_shape
+
+    # Extract a 2D surface mesh from a 3D volume (F=0)
+    verts, faces = measure.marching_cubes_classic(surf, 0.0, spacing=(0.1, 0.1, 0.1))
+
+
+    # Create a 3D figure
+    fig = plt.figure(figsize=(12,8))
+    ax = fig.add_subplot(111, projection='3d')
+
+    # Plot the surface
+    ax.plot_trisurf(verts[:, 0], verts[:,1], faces, verts[:, 2],
+                    cmap='Spectral', lw=1)
+
+    # Change the angle of view and title
+    ax.view_init(15, -15)
+
+    # ax.set_title(u"Made with ❤ (and Python)", fontsize=15) # if you have Python 3
+    ax.set_title("Made with <3 (and Python)", fontsize=15)
+
+    # Show me some love ^^
+    plt.show()
+
+if __name__ == '__main__':
+    main()
